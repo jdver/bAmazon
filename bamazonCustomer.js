8a5914658537874e528e3ccd267a2d6b886cf657
JD Vera <jdvera1@gmail.com>
2019-06-20 13:11:45 -0700

    working logic code

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,11 +7,13 @@
 // If answer === false, console.log "Insufficient" and STOP process
 // If response === true, update DataBase to reflect new remaining quantity
 // Response to client total order/purchase cost
+// DEPENDENCIES
 
 var mysql = require('mysql')
 var inquirer = require('inquirer')
 var Table = require('cli-table')
 
+// Establishing connection to mySQL
 var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
@@ -23,10 +25,9 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err
-  // console.log('Conncted as id: ' + connection.threadId)
-  // saleItems()
-  //  start()
 })
+
+// Display available items for sale
 var saleItems = function () {
   connection.query('SELECT * FROM products', function (err, res) {
     if (err) throw err
@@ -46,26 +47,56 @@ var saleItems = function () {
     }
     console.log(table.toString())
     console.log('')
-    // saleItems()
+    shop()
   })
 }
 
+// Prompt user to input sku number for item
+var shop = function () {
+  inquirer.prompt({
+    name: 'choice',
+    type: 'input',
+    message: 'Please enter the sku number for the item you would like to purchase'
+  })
+    .then(function (input) {
+      var item = input.choice
+      connection.query('SELECT * FROM products WHERE sku=?', item, function (err, res) {
+        if (err) throw err
+        if (res.length === 0) {
+          console.log('Please enter a sku number from the items available')
+          //  shop()
 
+          shop()
+        } else {
+          inquirer.prompt({
+            name: 'quantity',
+            type: 'input',
+            message: 'How many would you like to buy?'
+          })
+            .then(function (input2) {
+              var quantity = input2.quantity
+              if (quantity > res[0].stock_quantity) {
+                console.log('Out of stock!')
+                shop()
+              } else {
+                console.log('')
+                console.log(res[0].product_name + ' purchased')
+                console.log(quantity + ' qty @ $' + res[0].price)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+                var updatedQuantity = res[0].stock_quantity - quantity
+                connection.query('UPDATE products SET stock_quantity = ' + updatedQuantity + ' WHERE sku = ' + res[0].sku, function (err, res) {
+                  if (err) throw err
+                  console.log('')
+                  console.log('Your order has been processed!')
+                  console.log('Thank you!')
+                  console.log('')
+                  connection.end()
+                })
+              }
+            })
+        }
+      })
+    })
+}
 
 saleItems()
-
-
